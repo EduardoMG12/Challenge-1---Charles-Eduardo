6d9c9ac49e5360b041d992fb51c18061f3e33fc2
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-03-14 18:33:08 -0300

    feat: fix validation and implemented message errors when you wrong insert input value

--- src/register/main.js
+++ src/register/main.js
@@ -1,14 +1,23 @@
 var formularyContainer = document.querySelector("#formulary-container-inputs-register");
-var firstNameInput = document.getElementById("first-name"); // check
-var lastNameInput = document.getElementById("last-name"); // check
-var birthDateInput = document.getElementById("birth-date"); // check
+var firstNameInput = document.getElementById("first-name");
+var lastNameInput = document.getElementById("last-name");
+var birthDateInput = document.getElementById("birth-date");
 var countryInput = document.getElementById("country");
-var cityInput = document.getElementById("city"); // check
-var emailInput = document.getElementById("e-mail"); // check
-var passwordInput = document.getElementById("password"); // check
-var repeatPasswordInput = document.getElementById("repeat-password"); // check
+var cityInput = document.getElementById("city");
+var emailInput = document.getElementById("e-mail");
+var passwordInput = document.getElementById("password");
+var repeatPasswordInput = document.getElementById("repeat-password");
+var firstNameError = document.getElementById("firstname-error");
+var lastNameError = document.getElementById("lastname-error");
+var birthDateError = document.getElementById("birthdate-error");
+var countryError = document.getElementById("country-error");
+var cityError = document.getElementById("city-error");
+var emailError = document.getElementById("email-error");
+var passwordError = document.getElementById("password-error");
+var confirmPasswordError = document.getElementById("confirm-password-error");
 formularyContainer.onsubmit = function (event) {
     event.preventDefault();
+    console.log(formularyRegisterValidation());
     if (formularyRegisterValidation()) {
         var userDataRegistered = {
             firstName: firstNameInput.value,
@@ -19,57 +28,114 @@ formularyContainer.onsubmit = function (event) {
             email: emailInput.value,
             password: passwordInput.value,
         };
+        localStorage.clear();
         localStorage.setItem("user", JSON.stringify(userDataRegistered));
+        window.location.href = "/";
+        // remember fix errors span don't visual
+        // remember implementation localStorage in login window
+        // rememeber translate errors message to english
     }
 };
 function formularyRegisterValidation() {
-    if (!(isValidName(firstNameInput, lastNameInput), isValidDate(birthDateInput), isValidCountryAndCity(countryInput, cityInput), isValidEmail(emailInput), isValidPassword(passwordInput, repeatPasswordInput)))
-        false;
+    if (!(isValidName(firstNameInput, firstNameError, lastNameInput, lastNameError) && isValidDate(birthDateInput, birthDateError) && isValidCountryAndCity(countryInput, countryError, cityInput, cityError) && isValidEmail(emailInput, emailError) && isValidPassword(passwordInput, passwordError, repeatPasswordInput, confirmPasswordError))) {
+        console.log("passei no false");
+        return false;
+    }
+    console.log("passei no true");
     return true;
 }
-function isValidInput(input) {
-    if (input.value.trim().length === 0)
-        false;
-    else if (input.value.trim() !== input.value)
-        false;
+function isValidInput(input, span) {
+    if (input.value.trim().length === 0) {
+        showLabelError(span, "este campo é obrigatoro");
+        return false;
+    }
+    else if (input.value.trim() !== input.value) {
+        showLabelError(span, "não se pode ter espaço antes e nem depois desse campo");
+        return false;
+    }
+    hiddenLabelError(span);
     return true;
 }
-function isValidPassword(password, confirmPassword) {
-    if (!(isValidInput(password) && isValidInput(confirmPassword)))
+function isValidPassword(password, passwordError, confirmPassword, confirmPasswordError) {
+    if (!(isValidInput(password, passwordError) && isValidInput(confirmPassword, confirmPasswordError)))
         false;
-    if (!(password === confirmPassword))
-        false;
-    if (!(8 > password.value.length || password.value.length > 128))
-        false;
+    if (!(password.value === confirmPassword.value)) {
+        showLabelError(confirmPasswordError, "as senhas não coincidem");
+        return false;
+    }
+    if ((8 > password.value.length || password.value.length > 128)) {
+        showLabelError(confirmPasswordError, "a sua senha deve ter no mínimo 8 caracteres e no máximo 128 caracteres");
+        return false;
+    }
+    hiddenLabelError(passwordError);
+    hiddenLabelError(confirmPasswordError);
+    console.log("isValidPassword");
     return true;
 }
-function isValidName(firstName, lastName) {
+function isValidName(firstName, firstNameError, lastName, lastNameError) {
     var regexJustLetters = /^[a-zA-Z]+$/i;
-    if (!(isValidInput(firstName) && isValidInput(lastName)))
+    if (!(isValidInput(firstName, firstNameError) && isValidInput(lastName, lastNameError)))
         false;
-    if (!(regexJustLetters.test(firstName.value) && regexJustLetters.test(lastName.value)))
-        false;
+    if (!(regexJustLetters.test(firstName.value) && regexJustLetters.test(lastName.value))) {
+        showLabelError(firstNameError, "O seu nome de usuário não pode ter números e nem caracteres especiais.");
+        showLabelError(lastNameError, "O seu nome de usuário não pode ter números e nem caracteres especiais.");
+        return false;
+    }
+    hiddenLabelError(firstNameError);
+    hiddenLabelError(lastNameError);
+    console.log("isValidName");
     return true;
 }
-function isValidDate(birthDate) {
-    if (!(isValidInput(birthDate)))
+function isValidDate(birthDate, birthDateError) {
+    if (!(isValidInput(birthDate, birthDateError)))
         false;
-    if (!(new Date(birthDate.value).toLocaleDateString('pt-BR')))
-        false;
+    if (!(new Date(birthDate.value).toLocaleDateString('pt-BR'))) {
+        showLabelError(birthDateError, "Esta data não é válida");
+        return false;
+    }
+    hiddenLabelError(birthDateError);
+    console.log("isValidDate");
     return true;
 }
-function isValidEmail(email) {
+function isValidEmail(email, emailError) {
     var regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
-    if (!(regexEmail.test(email.value)))
-        false;
+    if (!(regexEmail.test(email.value))) {
+        showLabelError(emailError, "Este email não é valido");
+        return false;
+    }
+    hiddenLabelError(emailError);
+    console.log("isValidEmail");
     return true;
 }
-function isValidCountryAndCity(country, city) {
+function isValidCountryAndCity(country, countryError, city, cityError) {
     var regexCity = /^[a-zA-Z]+$/i;
     var regexCountry = /^[A-Za-z]{2}$/;
-    if (!(regexCountry.test(country.value.toUpperCase())))
+    if (!(isValidInput(city, cityError)))
         false;
-    if (!(regexCity.test(city.value)))
+    if (!(isValidInput(country, countryError)))
         false;
+    if (!(regexCountry.test(country.value.toUpperCase()))) {
+        showLabelError(countryError, "Esta estado não existe");
+        return false;
+    }
+    if (!(regexCity.test(city.value))) {
+        showLabelError(cityError, "Esta cidade não existe");
+        return false;
+    }
+    hiddenLabelError(countryError);
+    hiddenLabelError(cityError);
+    console.log("isValidCountryCity");
     return true;
+}
+function showLabelError(span, message) {
+    span.innerHTML = message;
+    span.style.display = 'block';
+    span.style.visibility = 'visible';
+    span.classList.add("label-error-active");
+}
+function hiddenLabelError(span) {
+    span.innerHTML = "";
+    span.style.display = 'none';
+    span.style.visibility = 'hidden';
+    span.classList.remove("label-error-active");
 }
--- src/register/main.ts
+++ src/register/main.ts
@@ -1,3 +1,8 @@
+// remember fix error span: i don't see spans together
+// remember i will implement localStorage in login window with username and email
+// rememeber translate errors message to english
+// remember implement key enter for send form and will fix svg animation to login window
+
 let formularyContainer = document.querySelector("#formulary-container-inputs-register")! as HTMLFormElement
 
 let firstNameInput = document.getElementById("first-name") as HTMLInputElement
@@ -9,6 +14,14 @@ let emailInput = document.getElementById("e-mail") as HTMLInputElement
 let passwordInput = document.getElementById("password") as HTMLInputElement
 let repeatPasswordInput = document.getElementById("repeat-password") as HTMLInputElement
 
+let firstNameError = document.getElementById("firstname-error") as HTMLSpanElement
+let lastNameError = document.getElementById("lastname-error") as HTMLSpanElement
+let birthDateError = document.getElementById("birthdate-error") as HTMLSpanElement
+let countryError = document.getElementById("country-error") as HTMLSpanElement
+let cityError = document.getElementById("city-error") as HTMLSpanElement
+let emailError = document.getElementById("email-error") as HTMLSpanElement
+let passwordError = document.getElementById("password-error") as HTMLSpanElement
+let confirmPasswordError = document.getElementById("confirm-password-error") as HTMLSpanElement
 
 interface IUser {
     firstName: string,
@@ -23,6 +36,7 @@ interface IUser {
 formularyContainer!.onsubmit = (event: Event) => {
     event.preventDefault();
 
+    console.log(formularyRegisterValidation())
     if (formularyRegisterValidation()) {
 
         const userDataRegistered: IUser = {
@@ -37,64 +51,121 @@ formularyContainer!.onsubmit = (event: Event) => {
         }
         localStorage.clear()
         localStorage.setItem("user", JSON.stringify(userDataRegistered))
+        window.location.href = "/"
     }
 }
 
 function formularyRegisterValidation(): boolean {
-    if (!(isValidName(firstNameInput, lastNameInput), isValidDate(birthDateInput), isValidCountryAndCity(countryInput, cityInput), isValidEmail(emailInput), isValidPassword(passwordInput, repeatPasswordInput))) false
-
+    if (!(isValidName(firstNameInput, firstNameError, lastNameInput, lastNameError) && isValidDate(birthDateInput, birthDateError) && isValidCountryAndCity(countryInput, countryError, cityInput, cityError) &&  isValidEmail(emailInput, emailError) && isValidPassword(passwordInput, passwordError, repeatPasswordInput, confirmPasswordError))){ 
+        console.log("passei no false")
+        return false
+    }
+    console.log("passei no true")
     return true
 }
 
-function isValidInput(input: HTMLInputElement): boolean {
+function isValidInput(input: HTMLInputElement, span: HTMLSpanElement): boolean {
 
-    if (input.value.trim().length === 0) false
-
-    else if (input.value.trim() !== input.value) false
+    if (input.value.trim().length === 0) {
+        showLabelError(span, "este campo é obrigatoro")
+        return false
+    }
+    
+    else if (input.value.trim() !== input.value){ 
+        showLabelError(span, "não se pode ter espaço antes e nem depois desse campo")
+        return false
+    }
+    hiddenLabelError(span)
     return true
 
 }
 
-function isValidPassword(password: HTMLInputElement, confirmPassword: HTMLInputElement) {
+function isValidPassword(password: HTMLInputElement, passwordError: HTMLSpanElement, confirmPassword: HTMLInputElement, confirmPasswordError: HTMLSpanElement) {
 
-    if (!(isValidInput(password) && isValidInput(confirmPassword))) false
-    if (!(password === confirmPassword)) false
-    if (!(8 > password.value.length || password.value.length > 128)) false
-
+    if (!(isValidInput(password, passwordError) && isValidInput(confirmPassword, confirmPasswordError))) false
+    if (!(password.value === confirmPassword.value)) {
+        showLabelError(confirmPasswordError, "as senhas não coincidem")
+        return false}
+    if ((8 > password.value.length || password.value.length > 128)) {
+        showLabelError(confirmPasswordError, "a sua senha deve ter no mínimo 8 caracteres e no máximo 128 caracteres")    
+        return false
+    }
+    hiddenLabelError(passwordError)
+    hiddenLabelError(confirmPasswordError)
+    console.log("isValidPassword")
     return true
 }
 
-function isValidName(firstName: HTMLInputElement, lastName: HTMLInputElement): boolean {
+function isValidName(firstName: HTMLInputElement, firstNameError: HTMLSpanElement, lastName: HTMLInputElement, lastNameError: HTMLSpanElement): boolean {
     const regexJustLetters = /^[a-zA-Z]+$/i
 
-    if (!(isValidInput(firstName) && isValidInput(lastName))) false
-    if (!(regexJustLetters.test(firstName.value) && regexJustLetters.test(lastName.value))) false
-
+    if (!(isValidInput(firstName, firstNameError) && isValidInput(lastName, lastNameError))) false
+    if (!(regexJustLetters.test(firstName.value) && regexJustLetters.test(lastName.value))){ 
+        showLabelError(firstNameError, "O seu nome de usuário não pode ter números e nem caracteres especiais.")
+        showLabelError(lastNameError, "O seu nome de usuário não pode ter números e nem caracteres especiais.")
+        return false
+    }
+    hiddenLabelError(firstNameError)
+    hiddenLabelError(lastNameError)
+    console.log("isValidName")
     return true
 }
 
-function isValidDate(birthDate: HTMLInputElement): boolean {
-    if (!(isValidInput(birthDate))) false;
-    if (!(new Date(birthDate.value).toLocaleDateString('pt-BR'))) false
+function isValidDate(birthDate: HTMLInputElement, birthDateError: HTMLSpanElement): boolean {
+    if (!(isValidInput(birthDate, birthDateError))) false;
+    if (!(new Date(birthDate.value).toLocaleDateString('pt-BR'))) {
+        showLabelError(birthDateError, "Esta data não é válida")    
+        return false
+    }
+    hiddenLabelError(birthDateError)
+    console.log("isValidDate")
     return true
 }
 
-function isValidEmail(email: HTMLInputElement): boolean {
+function isValidEmail(email: HTMLInputElement, emailError: HTMLSpanElement): boolean {
     const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/
 
-    if (!(regexEmail.test(email.value))) false
-
+    if (!(regexEmail.test(email.value))){
+        showLabelError(emailError, "Este email não é valido")
+        return false
+    }
+    
+        hiddenLabelError(emailError)
+        console.log("isValidEmail")
     return true
 }
 
-function isValidCountryAndCity(country: HTMLInputElement, city: HTMLInputElement): boolean {
-
+function isValidCountryAndCity(country: HTMLInputElement, countryError: HTMLSpanElement, city: HTMLInputElement, cityError: HTMLSpanElement): boolean {
     const regexCity = /^[a-zA-Z]+$/i
     const regexCountry = /^[A-Za-z]{2}$/
 
+    if (!(isValidInput(city, cityError))) false;
+    if (!(isValidInput(country, countryError))) false;
+    if (!(regexCountry.test(country.value.toUpperCase()))){
+        showLabelError(countryError, "Esta estado não existe")    
+        return false
+    }
+    if (!(regexCity.test(city.value))){
+        showLabelError(cityError, "Esta cidade não existe")    
+        return false
+    }
 
-    if (!(regexCountry.test(country.value.toUpperCase()))) false
-    if (!(regexCity.test(city.value))) false
-
+    hiddenLabelError(countryError)
+    hiddenLabelError(cityError)
+    console.log("isValidCountryCity")
     return true
+}
+
+function showLabelError(span: HTMLSpanElement, message: string) {
+    span.innerHTML = message;
+    span.style.display = 'block';
+    span.style.visibility = 'visible';
+    span.classList.add("label-error-active")
+}
+
+function hiddenLabelError(span: HTMLSpanElement) {
+    span.innerHTML = "";
+    span.style.display = 'none';
+    span.style.visibility = 'hidden';
+    span.classList.remove("label-error-active")
 }
