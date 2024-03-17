da7911b78bef3ec1b96e3c3d61f6cd4296883be1
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-03-17 14:41:17 -0300

    feat: add logic button logout

--- src/home/main.js
+++ src/home/main.js
@@ -1,14 +1,11 @@
-const buttonLogout = document.querySelector(".button-logout") as HTMLElement;
-
+var buttonLogout = document.querySelector(".button-logout");
 //remember fix there down logic
 // add link uol in icons to home page
 // add counter
 // trying add hours-o-clock
 //trying add weather api
 // and if plenty of time don't forget implement login validation in home page, for example if user is loged don't show login page again, show only user click in logout
-
-
-buttonLogout!.onclick = () => {
+buttonLogout.onclick = function () {
     localStorage.clear();
-    window.location.href = "/"
-}
+    window.location.href = "/";
+};
