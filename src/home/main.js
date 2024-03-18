1af77c4f313bb8c55dc2c8c1b6b878a27120efcf
charles.eduardo.mello.guimaraes@hotmail.com <charles.eduardo.mello.guimaraes@hotmail.com>
2024-03-18 15:38:53 -0300

    feat(home / main.ts): add date and counter logic.

--- src/home/main.js
+++ src/home/main.js
@@ -1,11 +1,110 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var buttonLogout = document.querySelector(".button-logout");
-//remember fix there down logic
-// add link uol in icons to home page
-// add counter
-// trying add hours-o-clock
+var fieldSeconds = document.querySelector(".seconds-number");
+var fieldHoursAClock = document.querySelector(".hours-a-clock");
+var fieldDate = document.querySelector(".hours-a-clock-description");
 //trying add weather api
 // and if plenty of time don't forget implement login validation in home page, for example if user is loged don't show login page again, show only user click in logout
+var daysWeek = [
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+    "Domingo"
+];
+var months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+];
 buttonLogout.onclick = function () {
     localStorage.clear();
     window.location.href = "/";
 };
+function updateaHours() {
+    var hours = new Date().getHours();
+    var minutes = new Date().getMinutes();
+    fieldHoursAClock.innerHTML = "".concat(hours, " : ").concat(minutes);
+    var month = new Date().getMonth();
+    var day = new Date().getDate();
+    var weekDay = new Date().getDay();
+    var year = new Date().getFullYear();
+    fieldDate.innerHTML = "".concat(daysWeek[weekDay].toLowerCase(), ", ").concat(day, " de  ").concat(months[month].toLowerCase(), " de ").concat(year);
+}
+setInterval(updateaHours, 60 * 1000);
+var wait = function (time) { return new Promise(function (res) { return setTimeout(res, time); }); };
+function startCounter(starting_1) {
+    return __awaiter(this, arguments, void 0, function (starting, delay) {
+        var i;
+        if (delay === void 0) { delay = 1000; }
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    i = starting;
+                    _a.label = 1;
+                case 1:
+                    if (!(i > 0)) return [3 /*break*/, 4];
+                    fieldSeconds.innerHTML = "".concat(i);
+                    return [4 /*yield*/, wait(delay)];
+                case 2:
+                    _a.sent();
+                    _a.label = 3;
+                case 3:
+                    i--;
+                    return [3 /*break*/, 1];
+                case 4: return [4 /*yield*/, startCounter(starting, delay)];
+                case 5:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+updateaHours();
+startCounter(60, 1000);
--- src/home/main.ts
+++ src/home/main.ts
@@ -1,14 +1,67 @@
 const buttonLogout = document.querySelector(".button-logout") as HTMLElement;
+const fieldSeconds = document.querySelector(".seconds-number") as HTMLElement
+const fieldHoursAClock = document.querySelector(".hours-a-clock") as HTMLElement
+const fieldDate = document.querySelector(".hours-a-clock-description") as HTMLElement
 
-//remember fix there down logic
-// add link uol in icons to home page
-// add counter
-// trying add hours-o-clock
 //trying add weather api
 // and if plenty of time don't forget implement login validation in home page, for example if user is loged don't show login page again, show only user click in logout
+
+const daysWeek = [
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+    "Domingo"
+
+]
+const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+]
 
 
 buttonLogout!.onclick = () => {
     localStorage.clear();
     window.location.href = "/"
 }
+
+function updateaHours() {
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    fieldHoursAClock.innerHTML = `${hours} : ${minutes}`
+    
+
+    const month = new Date().getMonth()
+    const day = new Date().getDate()
+    const weekDay = new Date().getDay()
+    const year = new Date().getFullYear()
+    fieldDate.innerHTML = `${daysWeek[weekDay].toLowerCase()}, ${day} de  ${months[month].toLowerCase()} de ${year}`
+
+}
+
+setInterval(updateaHours, 60 * 1000)
+
+const wait = (time: number) => new Promise(res => setTimeout(res, time))
+
+async function startCounter(starting: number, delay: number = 1000) {
+    for (let i = starting; i > 0; i--) {
+        fieldSeconds.innerHTML = `${i}`;
+        await wait(delay)
+    }
+    await startCounter(starting, delay)
+}
+
+updateaHours()
+startCounter(60, 1000)
